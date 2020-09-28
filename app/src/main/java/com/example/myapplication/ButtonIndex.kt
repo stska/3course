c4a266f918516e74a891da36cd2f4b0982cfdd07
stska <33188480+stska@users.noreply.github.com>
2020-09-28 22:36:25 +0300

    1lesson
    Я не знаю котлин,поэтому все что смог)

---
+++ app/src/main/java/com/example/myapplication/ButtonIndex.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+enum class ButtonIndex(val index: Int) {
+    FIRST(R.id.btn_counter1),
+    SECOND(R.id.btn_counter2),
+    THIRD( R.id.btn_counter3)
+}
---
+++ app/src/main/java/com/example/myapplication/view/MainView.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication.view
+
+interface MainView {
+    fun setButtonText(index : Int, text : String)
+}
