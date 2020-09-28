9dd6065cf0ccf3e960a072c43ca53c0c950c3dd7
stska <33188480+stska@users.noreply.github.com>
2020-09-28 22:37:22 +0300

    Revert "Revert "1lesson""
    This reverts commit b2e7e19dd76e7c5eb8bcc8cdc413b9620b8f603c.

---
+++ app/src/main/java/com/example/myapplication/model/CounterModel.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication.model
+
+class CounterModel {
+    val counters = mutableListOf(0, 0, 0)
+
+    fun getCurrent(index: Int): Int {
+        return counters[index]
+    }
+
+    fun next(index: Int): Int {
+        counters[index]++
+        return getCurrent(index)
+    }
+
+    fun set(index: Int, value: Int){
+        counters[index] = value
+    }
+
+
+}
