b2e7e19dd76e7c5eb8bcc8cdc413b9620b8f603c
stska <33188480+stska@users.noreply.github.com>
2020-09-28 22:36:55 +0300

    Revert "1lesson"
    This reverts commit c4a266f918516e74a891da36cd2f4b0982cfdd07.

--- app/src/main/java/com/example/myapplication/ButtonIndex.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication
-
-enum class ButtonIndex(val index: Int) {
-    FIRST(R.id.btn_counter1),
-    SECOND(R.id.btn_counter2),
-    THIRD( R.id.btn_counter3)
-}
--- app/src/main/java/com/example/myapplication/view/MainView.kt
+++
@@ -1,5 +0,0 @@
-package com.example.myapplication.view
-
-interface MainView {
-    fun setButtonText(index : Int, text : String)
-}
